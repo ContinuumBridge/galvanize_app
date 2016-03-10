e4b4d60c03f13b35dc14c5bf485463089709b09e
PeterClaydon <peter.claydon@continuumbridge.com>
2016-03-10 12:21:52 +0000

    Changed name to spur and enhanced functionality

--- spur_app.py
+++ spur_app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# galvanize_app.py
+# spur_app.py
 """
 Copyright (c) 2015 ContinuumBridge Limited
 """
 import sys
-from galvanize_app_a import App
+from spur_app_a import App
 App(sys.argv)
